bf8a38d65f572804dd769b09436970ba6e76dc3b
eshanganju <ganju.eshan@gmail.com>
2024-03-25 21:22:40 -0400

    Updated version of code

--- AndurilAnalyses.py
+++ AndurilAnalyses.py
@@ -1,20 +1,41 @@
 """This pipeline is for the analyses of the anduril particles.
 """
 
+import sys
+sys.path.insert(0,'/home/crg/Documents/Code/pacMan/')
+
 import tifffile as t
-from pac import Filter
 from pac import Segment
 from pac import Measure 
 
 
-binDataName = "/home/crg/Documents/Datasets/Anduril/Sample1/Sample1_Bin-Closed.tif"
+ofl = '/home/crg/Documents/Datasets/Anduril/output/'
+sampleName = 'Sample3-Test_06'
+sampleLoc = '/home/crg/Documents/Datasets/Anduril/Sample3/Scan3-Clean-1-Bin-Test.tif'
 
-edm_image = Segment.obtainEuclidDistanceMap()
-edm_peaks = Segment.obtainLocalMaximaMarkers()
-segmented_image = Segment.segmentUsingWatershed(	binaryMapToSeg = binDataName,
-							
-							
-						)
-corrected_segmented_image = Segment.fixErrorsInSegmentation()
+binData = t.imread(sampleLoc)
 
+edm_image = Segment.obtainEuclidDistanceMap(binData)
 
+edm_peaks = Segment.obtainLocalMaximaMarkers(edm_image)
+
+segmented_image = Segment.segmentUsingWatershed(binaryMapToSeg = binData, 
+												edmMapForTopo = edm_image,
+												edmPeaksForSeed = edm_peaks, 
+												addWatershedLine = False, 
+												sampleName = sampleName,
+												saveImg = True,
+												outputDir = ofl 
+												)
+
+corrected_segmented_image = Segment.fixErrorsInSegmentation(labelledMapForOSCorr = segmented_image, 
+															pad=2, 
+															areaLimit = 700, 
+															considerEdgeLabels=True,
+															checkForSmallParticles = True, 
+															voxelVolumeThreshold=1000,
+															radiusCheck=True, 
+															radiusRatioLimit=0.6, 
+															sampleName=sampleName, 
+															saveImg=True, 
+															outputDir=ofl)
